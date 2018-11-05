9671d5ebdd901531c44b1fc63357af9e8ba43661
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-11-05 18:35:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,17 +14,7 @@ class Dog
   
   ####
   
-  def initialize(breed)
-    @breed = breed
-  end
- 
-  def breed
-    @breed
-  end
- 
-  def breed=(new_breed)
-    @breed = new_breed
-  end
+  
   
 end
 
