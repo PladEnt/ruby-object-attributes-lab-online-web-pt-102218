66bde4f11da2079b93a59f1b0b8c962bba1ab179
Joshua D. Forrest <joshuadforrest@gmail.com>
2018-11-05 18:31:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,5 +12,18 @@ class Dog
     @name = new_name
   end
   
+  ####
+  
+  def initialize(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
+ 
+  def breed=(new_breed)
+    @breed = new_breed
+  end
   
 end
